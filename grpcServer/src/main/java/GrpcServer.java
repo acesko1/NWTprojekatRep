a828e841996236f7d14494f1d9a25484d2558623
acesko1 <acesko1@etf.unsa.ba>
2020-05-08 08:32:15 +0200

    Add files via upload
    grpc Server dodan, ali nije implementiran

---
+++ grpcServer/src/main/java/GrpcServer.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import user.UserService;
+
+public class GrpcServer {
+
+	 public static void main(String args[]) throws IOException, InterruptedException {
+		 
+		 System.out.println("Pokretanje GRPC servera");
+		 Server server = ServerBuilder.forPort(8085).addService(
+
+				 new UserServiceApplication()).build();
+		 server = ServerBuilder.forPort(8084).addService(
+
+				 new SupplyServiceApplication()).build();
+		 server = ServerBuilder.forPort(8083).addService(
+
+				 new PaymentServiceApplication()).build();
+		 server = ServerBuilder.forPort(8082).addService(
+
+				 new FeedbackServiceApplication()).build();
+		 server = ServerBuilder.forPort(8081).addService(
+
+				 new AuthorizationServiceApplication()).build();
+		 
+		 server.start();
+		 System.out.println("Server pokrenut na "+ server.getPort());
+	        server.awaitTermination();
+	 }
+}
